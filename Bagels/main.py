85daa165c03567c2d3a260259cdddf2cb117ff2e
Mohamed El Ajouaoui <Mohamedelajouaoui99@gmail.com>
2024-07-13 20:39:57 +0200

    Überprüfung des Inputs durch while-loop

--- Bagels/main.py
+++ Bagels/main.py
@@ -4,7 +4,7 @@ NUM_DIGITS = 3
 MAX_GUESSES = 10
 
 
-def generate_three_digits():
+def generate_secret_num():
     hidden_three_digit = []
     for i in range(3):
         random_number = random.randint(0, 9)
@@ -23,8 +23,19 @@ def main():
     # I have thought up a number.
     # You have 10 guesses to get it.""")
 
-    for i in range(10):
-        hidden_three_digits = generate_three_digits()
-        i += 1
-        guess = input(f"Guess #{i}:\n")
-        guess_three_digits = [int(digit) for digit in guess]
+    while True:
+        secret_num = generate_secret_num()
+        print("I have thought up a Number.")
+        print(f"You have {MAX_GUESSES} guesses to get it.")
+
+        num_guesses = 1
+        while num_guesses <= MAX_GUESSES:
+            guess = ""
+            #Checking the Input, before saving it.
+            while len(guess) != NUM_DIGITS or not guess.isdecimal():
+                print(f"Guess {num_guesses}")
+                guess = input("> ")
+
+
+if __name__ == '__main__':
+    main()
