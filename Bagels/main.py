81fb67286602e2e348c5200d6054c6e027f2ba1e
Mohamed El Ajouaoui <Mohamedelajouaoui99@gmail.com>
2024-07-13 22:03:28 +0200

    Verbesserungen

--- Bagels/main.py
+++ Bagels/main.py
@@ -35,14 +35,12 @@ def get_clues(guess, secret_num):
 
 def main():
 
-    # print("""I am thinking of a 3-digit number. Try to guess what it is.
-    # Here are some clues:
-    # When I say:      That means:
-    #  Pico           One digit is correct but in the wrong position.
-    #  Fermi          One digit is correct and in the right position.
-    #  Bagels         No digit is correct.
-    # I have thought up a number.
-    # You have 10 guesses to get it.""")
+    print("""I am thinking of a 3-digit number. Try to guess what it is.
+Here are some clues:
+When I say:      That means:
+    Pico           One digit is correct but in the wrong position.
+    Fermi          One digit is correct and in the right position.
+    Bagels         No digit is correct.""")
 
     while True:
         secret_num = generate_secret_num()
