ae1a43b2b655000dc41de17595038de1d225095f
Mohamed El Ajouaoui <Mohamedelajouaoui99@gmail.com>
2024-07-13 17:07:47 +0200

    main()

--- Bagels/main.py
+++ Bagels/main.py
@@ -1,18 +1,31 @@
 import random
 
-print("""I am thinking of a 3-digit number. Try to guess what it is.
-Here are some clues:
-When I say:      That means:
-  Pico           One digit is correct but in the wrong position.
-  Fermi          One digit is correct and in the right position.
-  Bagels         No digit is correct.
-I have thought up a number.
- You have 10 guesses to get it.""")
+NUM_DIGITS = 3
+MAX_GUESSES = 10
 
 
 def generate_three_digits():
-    three_digit_number = []
+    hidden_three_digit = []
     for i in range(3):
         random_number = random.randint(0, 9)
-        three_digit_number.append(random_number)
-    print(three_digit_number)
+        hidden_three_digit.append(str(random_number))
+    return hidden_three_digit
+
+
+def main():
+
+    # print("""I am thinking of a 3-digit number. Try to guess what it is.
+    # Here are some clues:
+    # When I say:      That means:
+    #  Pico           One digit is correct but in the wrong position.
+    #  Fermi          One digit is correct and in the right position.
+    #  Bagels         No digit is correct.
+    # I have thought up a number.
+    # You have 10 guesses to get it.""")
+
+    for i in range(10):
+        hidden_three_digits = generate_three_digits()
+        i += 1
+        guess = input(f"Guess #{i}:\n")
+        guess_three_digits = [int(digit) for digit in guess]
+        #Exception schreiben falls keine Zahlen getippt wurden.
