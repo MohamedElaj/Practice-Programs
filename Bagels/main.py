fb56cd00c179678217ad9c1001c683fb9f28d4c9
Mohamed El Ajouaoui <Mohamedelajouaoui99@gmail.com>
2024-07-13 21:06:01 +0200

    definieren get_clues()

--- Bagels/main.py
+++ Bagels/main.py
@@ -16,3 +16,23 @@ def generate_three_digits():
         random_number = random.randint(0, 9)
         three_digit_number.append(random_number)
     print(three_digit_number)
+
+
+def get_clues(guess, secret_num):
+    """Returns a string. On every guess we add a clue."""
+    if guess == secret_num:
+        return "You got it!"
+
+    clues = []
+
+    for i in range(len(guess)):
+        if guess[i] == secret_num[i]:
+            clues.append("Fermi")
+        elif guess[i] in secret_num:
+            clues.append("Pico")
+
+    if len(clues) == 0:
+        return "Bagels"
+    else:
+        clues.sort()
+        return " ".join(clues)
