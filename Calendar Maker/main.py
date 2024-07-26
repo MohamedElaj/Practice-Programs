07a69286680dc88718657f42c1501ba34bba4ed8
Mohamed El Ajouaoui <Mohamedelajouaoui99@gmail.com>
2024-07-26 10:19:21 +0200

    ascII erstellt und die Input Variablen

--- Calendar Maker/main.py
+++ Calendar Maker/main.py
@@ -1,3 +1,22 @@
-from art import asc_art
+import art
+import datetime
+
+
+month = ()
+
+while True:
+    response:str = input("Enter the year of the calender: \n> ")
+    if response.isdecimal() and (0 < int(response) <= 9999):
+        year = response
+        break
+
+
+while True:
+    response: str = input("Enter the month of the calendar, 1-12: \n> ")
+    if response.isdecimal() and (1 <= int(response) <= 12):
+        month = int(response)
+        # Hier vielleicht durch eine Dictionary, den richtigen Monat rauspicken durch die angegebene Zahl.
+        break
+
 
 
