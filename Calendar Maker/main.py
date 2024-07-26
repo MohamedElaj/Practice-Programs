e6a6603349a1c1ed0181a05a0055bcd8000c76e7
Mohamed El Ajouaoui <Mohamedelajouaoui99@gmail.com>
2024-07-26 11:42:30 +0200

    Clean Code

--- Calendar Maker/main.py
+++ Calendar Maker/main.py
@@ -2,21 +2,23 @@ import art
 import datetime
 
 
-month = ()
+def main() -> None:
+    month = ()
 
-while True:
-    response:str = input("Enter the year of the calender: \n> ")
-    if response.isdecimal() and (0 < int(response) <= 9999):
-        year = response
-        break
-
-
-while True:
-    response: str = input("Enter the month of the calendar, 1-12: \n> ")
-    if response.isdecimal() and (1 <= int(response) <= 12):
-        month = int(response)
-        # Hier vielleicht durch eine Dictionary, den richtigen Monat rauspicken durch die angegebene Zahl.
-        break
+    while True:
+        response:str = input("Enter the year of the calender: \n> ")
+        if response.isdecimal() and (0 < int(response) <= 9999):
+            year = response
+            break
 
 
+    while True:
+        response: str = input("Enter the month of the calendar, 1-12: \n> ")
+        if response.isdecimal() and (1 <= int(response) <= 12):
+            month = int(response)
+            # Hier vielleicht durch eine Dictionary, den richtigen Monat rauspicken durch die angegebene Zahl.
+            break
+
+
+main()
 
